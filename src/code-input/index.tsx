ad2b1316b6587e0d23452fac7dd80d0a041646a4
carrotpai <55906326+carrotpai@users.noreply.github.com>
2024-01-10 21:32:53 +0800

    feat: keyboard arrows и комментарии

--- src/code-input/index.tsx
+++ src/code-input/index.tsx
@@ -1,87 +1,102 @@
-import { useState } from "react"
+import { useState } from "react";
 import { InputCell } from "./ui";
 import useRefs from "./hooks/useRefs";
 
 interface CodeInputProps {
-    length: number;
-    onChange?: (value: string) => void;
-    onComplete?: (value: string) => void;
+  length: number;
+  onChange?: (value: string) => void;
+  onComplete?: (value: string) => void;
 }
 
+const generateCellKey = (ind: number) => `cell-${ind}`;
 
-const generateCellKey = (ind: number) => `cell-${ind}`
-
-
-
-
-function CodeInput({length}: CodeInputProps) {
+function CodeInput({ length }: CodeInputProps) {
   //значение инпута - массив длины Length (значения массива = значения клеток, изначально пустые строки)
-  const [value, setValue] = useState(Array(length).fill(''))
-  //рефы для навигации
-  const {getRefsMap, setRef} = useRefs<HTMLInputElement>()
+  const [value, setValue] = useState(Array(length).fill(""));
+  //рефы для навигации, представляют собой мап, где ключи это react-keys для клеток-инпутов
+  const { getRefsMap, setRef } = useRefs<HTMLInputElement>();
 
-  const handleOnFill = (ind: number) => {
-        const cellRefs = getRefsMap()
-        const nextCellInd = getNextCell(ind)
-        if (nextCellInd !== -1) {
-          cellRefs.get(generateCellKey(nextCellInd))?.focus()
-        }
-  }
+  const FocusNextCell = (
+    ind: number,
+    typeOfFocus: "find" | "incr" = "find",
+  ) => {
+    const cellRefs = getRefsMap();
+    const nextCellInd = {
+      find: getNextCell(ind),
+      incr: ind === length - 1 ? -1 : ind + 1,
+    };
+    if (nextCellInd[typeOfFocus] !== -1) {
+      cellRefs.get(generateCellKey(nextCellInd[typeOfFocus]))?.focus();
+    }
+  };
 
-  const handleOnDelete = (ind: number) => {
-        const cellRefs = getRefsMap()
-        if (ind !== 0) {
-          cellRefs.get(generateCellKey(ind - 1))?.focus()
-        }
-  }
+  const FocusPreviousCell = (ind: number) => {
+    const cellRefs = getRefsMap();
+    if (ind !== 0) {
+      cellRefs.get(generateCellKey(ind - 1))?.focus();
+    }
+  };
 
   //достать след не заполненную клетку
   const getNextCell = (ind: number) => {
-      for(let i = ind + 1; i < length; i++){
-        if (value[i] === '') {
-          return i
-        }
+    for (let i = ind + 1; i < length; i++) {
+      if (value[i] === "") {
+        return i;
       }
-      for(let i = 0; i < ind; i++){
-        if (value[i] === '') {
-          return i
-        }
+    }
+    for (let i = 0; i < ind; i++) {
+      if (value[i] === "") {
+        return i;
       }
-      return -1
-  }
-
+    }
+    return -1;
+  };
 
   //клетки
-  const cells: React.ReactNode[] = []
-  for(let ind = 0; ind < length; ind++){
+  const cells: React.ReactNode[] = [];
+  for (let ind = 0; ind < length; ind++) {
     cells.push(
       <InputCell
         index={ind}
         ref={(element) => setRef(generateCellKey(ind), element)}
         containerClassName="w-10 h-12 bg-slate-200 rounded-md"
         inputClassName="w-6 h-10"
-        onFilled={handleOnFill}
+        onFilled={(ind) => FocusNextCell(ind, "find")}
         //срабатывает при удалении при пустом значении ячейки (нейминг получше нужен, но мне чет в голову ниче толкового не приходит)
-        onDelete={handleOnDelete}
+        onDelete={FocusPreviousCell}
         onChange={(e) => {
           setValue((prev) => {
-            const newValue = Array.from(prev)
-            newValue[ind] = (e.target as HTMLInputElement).value
-            return newValue
-          })
+            const newValue = Array.from(prev);
+            newValue[ind] = (e.target as HTMLInputElement).value;
+            return newValue;
+          });
+        }}
+        onKeyDown={(e) => {
+          if (e.code === "ArrowLeft") {
+            FocusPreviousCell(ind);
+            /**нужен preventDefaul, чтобы после фокуса
+             * не происходило движение по тексту
+             * без него произойдет обработка нажатия стрелки
+             * для нового сфокусированного инпута
+             * и курсор будет переводить либо в начало ипута, либо в конец */
+            e.preventDefault();
+          }
+          if (e.code === "ArrowRight") {
+            FocusNextCell(ind, "incr");
+            e.preventDefault();
+          }
         }}
         value={value[ind]}
         key={generateCellKey(ind)}
-       />
-
-    )
+      />,
+    );
   }
 
   return (
     <div className="flex flex-row justify-between gap-3 w-fit h-fit">
-        {cells}
+      {cells}
     </div>
-  )
+  );
 }
 
-export default CodeInput
+export default CodeInput;
--- src/code-input/ui/input-cell/index.tsx
+++ src/code-input/ui/input-cell/index.tsx
@@ -1,50 +1,74 @@
-import React from "react"
+import React from "react";
 
-type CellStateChangeHandler = (ind: number) => void
+type CellStateChangeHandler = (ind: number) => void;
 
 interface InputCellProps {
   index: number;
-  value?: string
+  value?: string;
   containerClassName?: string;
   inputClassName?: string;
 
   onChange?: (e: React.ChangeEvent) => void;
+  onKeyDown?: (e: React.KeyboardEvent) => void;
   onFilled?: CellStateChangeHandler;
   onDelete?: CellStateChangeHandler;
 }
 
+const InputCell = React.forwardRef<HTMLInputElement, InputCellProps>(
+  function InputCell(props, ref) {
+    return (
+      <div
+        className={`${props.containerClassName} relative focus-within:border-[2px] focus-within:border-sky-400`}
+      >
+        <input
+          style={{
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+          className={`${props.inputClassName} absolute appearance-none bg-opacity-0 bg-transparent focus:outline-none text-center`}
+          type="text"
+          maxLength={1}
+          ref={ref}
+          value={props.value}
+          onKeyDown={(e) => {
+            if (props.onKeyDown) {
+              props.onKeyDown(e);
+            }
+            //value falsy значение + value != 0, нажали backspace -> вызываем onDelete
+            if (!props.value && e.key === "Backspace" && props.onDelete) {
+              props.onDelete(props.index);
+              /**
+               * Тут нету e.preventDefault, что означает,
+               * что сначало выполнится onDelete и .focus внутри него
+               * после .focus, document.activeElement станет равным нашей предыдущей клетке
+               * далее происхдит дальнейшая обработка поведения keyboard ивента (то что в preventDefault)
+               * тут происходит создание changeEvent для текущего активного элемента
+               * т.к. произошло нажатие "backspace" произойдет удаление 1 символа,
+               * что автоматически удаляет значение из предыдущей клетки
+               * не баг, а фича)
+               */
+            }
+          }}
+          onChange={(e) => {
+          //от 0 до 9 + удаление
+            if (
+              (e.target.value >= "0" && e.target.value <= "9") ||
+              !e.target.value
+            ) {
+              if (props.onChange) {
+                props.onChange(e);
+              }
+              // если это символы 0-9 то вызываем onFilled
+              if (e.target.value && props.onFilled) {
+                props.onFilled(props.index);
+              }
+            }
+          }}
+        />
+      </div>
+    );
+  },
+);
 
-const InputCell = React.forwardRef<HTMLInputElement, InputCellProps>(function InputCell(props, ref) {
-  return (
-    <div className={`${props.containerClassName} relative focus-within:border-[2px] focus-within:border-sky-400`}>
-      <input
-       style={{'top': '50%', left: '50%', transform: 'translate(-50%, -50%)'}}
-       className={`${props.inputClassName} absolute appearance-none bg-opacity-0 bg-transparent focus:outline-none text-center`}
-       type="text"
-       maxLength={1}
-       ref={ref}
-       value={props.value}
-       onKeyDown={(e) => {
-        //value falsy значение + value != 0, нажали backspace -> вызываем onDelete
-        if (!props.value && e.key === "Backspace" && props.onDelete) {
-          props.onDelete(props.index)
-        }
-       }}
-       onChange={(e) => {
-        //от 0 до 9 + удаление
-        if ((e.target.value >= '0' && e.target.value <= '9') ||  !e.target.value) {
-          if (props.onChange) {
-            props.onChange(e)
-          }
-          // если это символы 0-9 то вызываем onFilled
-          if (e.target.value && props.onFilled) {
-            props.onFilled(props.index)
-          }
-        }
-       }}
-      />
-    </div>
-  )
-})
-
-export default InputCell
+export default InputCell;
